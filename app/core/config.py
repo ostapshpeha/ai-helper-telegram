799eec67529036e86e2194c795f7a2a6269b3f2f
Ostap Shp <stark171705@gmail.com>
2026-02-24 18:54:28 +0200

    defined config

---
+++ app/core/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "Honda AI Assistant"
+    MONGO_DB_URL: str = "mongodb://localhost:27017"
+    MONGO_DB_USER: str = "honda_db"
+    MONGO_DB_PASSWORD: str = "password"
+
+    GEMINI_API_KEY: str = ""
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
