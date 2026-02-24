ba401202a5ff6599dce614cfedb0b90a6c1195a6
Ostap Shp <stark171705@gmail.com>
2026-02-24 18:54:39 +0200

    mongo ab init

---
+++ app/core/database.py
@@ -0,0 +1,20 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from beanie import init_beanie
+from app.core.config import settings
+from app.models.ligtning import ChatLog
+from app.models.service import Mechanic, ServiceSlot
+
+
+async def init_db():
+    client = AsyncIOMotorClient(settings.MONGO_DB_URL)
+    database = client[settings.MONGO_DB_USER]
+
+    await init_beanie(
+        database=database,
+        document_models=[
+            Mechanic,
+            ServiceSlot,
+            ChatLog
+        ]
+    )
+    print("Successfully connected to MongoDB!")
